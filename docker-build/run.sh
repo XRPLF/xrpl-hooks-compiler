9696c8b8d17f088b52fa192dbe528aa77ee1d2af
Vaclav Barta <vaclav@equilibrium.co>
2021-11-23 08:35:27 +0100

    noticing background process errors

--- docker-build/run.sh
+++ docker-build/run.sh
@@ -3,6 +3,10 @@
 echo running run.sh...
 set -e
 
+# what https://docs.docker.com/config/containers/multi-service_container/ calls "very naive"...
 cd /jsonrpc-ws-proxy && node dist/server.js --port 3001 --languageServers servers.yml &
 
-cd /c2wasm-api && node dist/index.js
+cd /c2wasm-api && node dist/index.js &
+
+wait -n
+exit $?
