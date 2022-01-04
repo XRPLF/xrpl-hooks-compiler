567c11bbcff1084c9fe3cded5b6a0170add08f87
Vaclav Barta <vbarta@mangrove.cz>
2022-01-04 08:14:23 +0100

    load test scripts

---
+++ load-test/check.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import sys
+
+class Demultiplex:
+    def __init__(self, log_name):
+        self.log_name = log_name
+        self.incoming_count = 0
+        self.outgoing_count = 0
+        self.request2count = {}
+        self.reply2count = {}
+        self.expected_multiple = {
+            '$/cancelRequest': 2,
+            'textDocument/didOpen': 2, # accept.c + hookapi.h
+            'textDocument/publishDiagnostics': 2
+        }
+
+    def parse(self, ln):
+        if ln.find("Forwarding new client") >= 0:
+            self.incoming_count += 1
+        elif ln.find("LSP finished, exiting with status") >= 0:
+            self.outgoing_count += 1
+        else:
+            self.parse_message(ln, self.request2count, "<--")
+            self.parse_message(ln, self.reply2count, "-->")
+
+    @staticmethod
+    def parse_message(ln, r2count, arrow):
+        pos = ln.find(arrow)
+        if pos >= 0:
+            tail = ln[(pos + len(arrow)):]
+            words = tail.split()
+            if len(words):
+                msg = words[0].strip()
+                if msg:
+                    cnt = r2count.get(msg, 0)
+                    r2count[msg] = 1 + cnt
+
+    def dump(self):
+        if self.incoming_count == self.outgoing_count:
+            print("%s: %d clients handled" % (self.log_name, self.incoming_count))
+        else:
+            raise Exception("%s: %d incoming, %d outgoing" % (self.log_name, self.incoming_count, self.outgoing_count))
+
+        self.dump_message_count(self.request2count, "<--")
+        self.dump_message_count(self.reply2count, "-->")
+
+    def dump_message_count(self, r2count, arrow):
+        for msg, cnt in sorted(r2count.items()):
+            mult = self.expected_multiple.get(msg, 1)
+            if cnt != (self.incoming_count * mult):
+                print("\t%d x %s %s" % (cnt, arrow, msg))
+
+
+def main():
+    for a in sys.argv[1:]:
+        demultiplex = Demultiplex(a)
+        with open(a) as f:
+            for ln in f:
+                demultiplex.parse(ln)
+
+        demultiplex.dump()
+
+
+if __name__ == "__main__":
+    main()
---
+++ load-test/drive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from selenium import webdriver
+from selenium.common import exceptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+class Driver:
+    def __init__(self):
+        options = webdriver.ChromeOptions();
+        options.add_argument("--start-maximized");
+        service = Service('/usr/lib/chromium-browser/chromedriver')
+        self.br = webdriver.Chrome(service=service, options=options)
+
+    def run(self):
+        self.br.execute_script('window.open("https://xrpl-hooks-ide.vercel.app/develop/be088224fb37c0075e84491da0e602c1", "_blank");')
+
+    def close(self):
+        self.br.close()
+
+
+def main():
+    drivers = []
+    for i in range(2):
+        driver = Driver()
+        driver.run()
+        drivers.append(driver)
+
+    time.sleep(60)
+
+    for driver in drivers:
+        driver.close()
+
+
+if __name__ == "__main__":
+    main()
